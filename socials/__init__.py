5e0aca8b0aca8cf1275f0fdb8bf3761fdbf6634e
lorey <git@karllorey.com>
2018-05-17 21:57:51 +0200

    Extraction class

--- socials/__init__.py
+++ socials/__init__.py
@@ -1,34 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for Socials."""
-import re
+from socials.socials import Extraction
 
 __author__ = """Karl Lorey"""
 __email__ = 'git@karllorey.com'
 __version__ = '0.1.0'
 
-PATTERNS = {
-    'facebook': [
-        'http(s)?://(www\.)?(facebook|fb)\.com/[A-z0-9_\-\.]+/?'
-    ],
-    'twitter': [
-        'http(s)?://(.*\.)?twitter\.com\/[A-z0-9_]+/?'
-    ],
-    'linkedin': [
-        'http(s)?://([\w]+\.)?linkedin\.com/in/(A-z0-9_-)\/?',
-        'http(s)?://([\w]+\.)?linkedin\.com/pub/[A-z0-9_-]+(\/[A-z 0-9]+){3}/?'
-    ],
-}
 
-
-def extract(urls: list):
-    matches = {}
-    for url in urls:
-        for plattform in PATTERNS:
-            is_match = any(re.match(p, url) for p in PATTERNS[plattform])
-            if is_match:
-                if plattform not in matches:
-                    matches[plattform] = []
-                matches[plattform].append(url)
-
-    return matches
+def extract(urls):
+    return Extraction(urls).get_matches_per_platform()
--- socials/socials.py
+++ socials/socials.py
@@ -1,3 +1,52 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import re
+
+PATTERNS = {
+    'facebook': [
+        'http(s)?://(www\.)?(facebook|fb)\.com/[A-z0-9_\-\.]+/?'
+    ],
+    'twitter': [
+        'http(s)?://(.*\.)?twitter\.com\/[A-z0-9_]+/?'
+    ],
+    'linkedin': [
+        'http(s)?://([\w]+\.)?linkedin\.com/in/(A-z0-9_-)\/?',
+        'http(s)?://([\w]+\.)?linkedin\.com/pub/[A-z0-9_-]+(\/[A-z 0-9]+){3}/?'
+    ],
+}
+
+
+class Extraction(object):
+    """Extracted profiles."""
+
+    _urls = None
+
+    def __init__(self, urls):
+        self._urls = urls
+
+    def get_matches_per_platform(self):
+        """
+        Get lists of profiles keyed by platform name.
+        :return: a dictionary with the platform as a key,
+        and a list of the platform's profiles as values
+        """
+        return extract_matches_per_platform(self._urls)
+
+
+def extract_matches_per_platform(urls):
+    """
+    Get lists of profiles keyed by platform name.
+    :return: a dictionary with the platform as a key,
+    and a list of the platform's profiles as values
+    """
+    matches = {}
+    for url in urls:
+        for plattform in PATTERNS:
+            is_match = any(re.match(p, url) for p in PATTERNS[plattform])
+            if is_match:
+                if plattform not in matches:
+                    matches[plattform] = []
+                matches[plattform].append(url)
+
+    return matches
